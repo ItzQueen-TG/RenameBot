2e77313aaf91b17da4fe7a59f63d7c19f5fd7fdf
Mуѕтєяισυѕ gιяℓ <102360292+ItzQueen-TG@users.noreply.github.com>
2023-08-04 18:31:16 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-async def edit_effect(query, button)
+async def edit_effect(query, button):
     await query.message.edit_text(
             text="",
             reply_markup=button,
