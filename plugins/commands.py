397c1934c38d2a31cfcc7668820e80aa3e64dd46
Mуѕтєяισυѕ gιяℓ <102360292+ItzQueen-TG@users.noreply.github.com>
2023-08-04 17:43:53 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,1 +1,16 @@
+from Script import script
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+
+@Client.on_message(filters.private & filters.command("start"))
+async def start(client, message):
+    user = message.from_user
+    button = InlineKeyboardMarkup([[
+        InlineKeyboardButton("Help & Settings", callback_data='help')
+        ],[
+        InlineKeyboardButton('🎟️ Upgrade', callback_data='upgrade'),
+        InlineKeyboardButton('🗣️ More', callback_data='more')
+    ]])
+    await message.reply_text(text=script.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
+   
